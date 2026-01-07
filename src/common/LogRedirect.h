4525aeb39419da814b43fd86b66cf8b34c4fcaff
showshowmaker <1642512766@qq.com>
2026-01-07 14:23:31 +0800

    feat:real node

--- src/common/LogRedirect.h
+++ src/common/LogRedirect.h
@@ -6,10 +6,10 @@
 
 inline bool RedirectLogs(const std::string& path) {
     if (path.empty()) return true;
-    if (!std::freopen(path.c_str(), "a", stdout)) {
+    if (!std::freopen(path.c_str(), "w", stdout)) {
         return false;
     }
-    if (!std::freopen(path.c_str(), "a", stderr)) {
+    if (!std::freopen(path.c_str(), "w", stderr)) {
         return false;
     }
     std::ios::sync_with_stdio(true);
